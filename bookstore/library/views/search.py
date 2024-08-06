aa48715f7020d0899d450ea970e967cfab642f71
Francisco Pieper <fapieper@miuandes.cl>
2024-08-06 11:28:43 -0400

    feat: searchbar working

--- bookstore/library/views/search.py
+++ bookstore/library/views/search.py
@@ -1,4 +1,21 @@
-from django.views.generic import TemplateView
+# myapp/views/search.py
+from django.shortcuts import render
+from django.views import View
+from django.core.paginator import Paginator
+from ..models import Book
 
-class SearchView(TemplateView):
+class SearchView(View):
     template_name = 'search/search.html'
+
+    def get(self, request):
+        query = request.GET.get('q')
+        if query:
+            search_results = Book.objects.filter(summary__icontains=query)
+        else:
+            search_results = Book.objects.none()
+
+        paginator = Paginator(search_results, 10)  # Show 10 books per page
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, self.template_name, {'search_results': page_obj})
